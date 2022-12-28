13c92d2350ca5ba56fe2da006ca66b7d1b79c8cb
JC <jaechol.hong@outlook.com>
2022-12-28 01:43:26 +0100

    03-4 리액트 기초 props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+function Food({ fav }) {
+  return <h3>I like {fav}</h3>
+}
+
 function App() {
-  return (<div><h1>Hello!!!</h1></div>)
+  return (
+    <div>
+      <h1>Hello!!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="steak" />
+      <Food fav="wine" />
+      <Food fav="beer" />
+    </div>)
 }
 
 export default App;
