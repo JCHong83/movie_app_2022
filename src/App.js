8d276cfa674baa3a1270e53246769ef3ab7752fd
JC <jaechol.hong@outlook.com>
2023-01-02 18:13:51 +0100

    props usage

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { findDOMNode } from 'react-dom';
 
-function Food({ name }) {
-  return <h3>I like {name}</h3>
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'udon',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fkisaki.it%2Fproduct%2Fwakame-udon%2F&psig=AOvVaw0t9FToqELoHd2F5bpVUFzh&ust=1672313290661000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjW59OYnPwCFQAAAAAdAAAAABAL',
+    image: 'https://www.pressurecookrecipes.com/wp-content/uploads/2021/07/udon-soup-recipe.jpg',
+    rating: 4,
   },
   {
+    id: 2,
     name: 'okonomiyaki',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.japan-guide.com%2Fr%2Fe100.html&psig=AOvVaw0GmWTVwBZj4xtE0Xjl677A&ust=1672313589832000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLih_uqZnPwCFQAAAAAdAAAAABAD',
+    image: 'https://www.japan-guide.com/g19/r100_01.jpg',
+    rating: 4.5,
   },
   {
+    id: 3,
     name: 'yakitori',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcooking.nytimes.com%2Frecipes%2F1013201-yakitori-grilled-chicken-skewers&psig=AOvVaw0W36jbbOHdEiHMRf5lu0gQ&ust=1672313711324000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPCSy5uanPwCFQAAAAAdAAAAABAD',
+    image: 'https://www.ohayo.it/wp-content/uploads/2018/12/yakitori-spiedini-pollo-giapponesi-ricetta.jpg',
+    rating: 4.4,
   },
   {
+    id: 4,
     name: 'fiorentina',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmonicucci.altervista.org%2Frecipe-items%2Fbistecca-alla-fiorentina-doc%2F&psig=AOvVaw0uEcMfYxQd4Mxa5LPuCqsa&ust=1672313824593000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLDtkceanPwCFQAAAAAdAAAAABAN',
+    image: 'https://www.braciamiancora.com/wp-content/uploads/2015/08/Bistecca-Fiorentina-Boccanegra-Firenze-5.jpg',
+    rating: 5,
   },
   {
+    id: 5,
     name: '곱창구이',
     image: 'https://s3.ap-northeast-2.amazonaws.com/event-localnaeil/FileData/Article/202005/d4bc58f6-c585-4a83-a09a-7a6e0e2dbe0e.jpg',
+    rating: 5,
   },
 ];
 
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (<Food name={dish.name} />))}
-    </div>)
-}
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
+    </div>
+  );
+};
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 
 export default App;
